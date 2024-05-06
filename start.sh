6928890c88074860e88b829fefbbcb625ceba0c3
Sayyed Faizan <103510430+SahilFaiz@users.noreply.github.com>
2024-05-06 12:45:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
-
-echo "Cloning main Repository"
-git clone https://github.com/SahilFaiz/Premium-Bots /DQ-The-File-Donor
-
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/SahilFaiz/Premium-Bots /DQ-The-File-Donor
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+fi
 cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
